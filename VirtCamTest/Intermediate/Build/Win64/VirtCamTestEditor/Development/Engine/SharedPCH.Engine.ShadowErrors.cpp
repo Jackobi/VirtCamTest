fdceb49d98aa740f60f12e6e530e84089111392b
Jackobi <jackobi.forsyth@gmail.com>
2021-09-22 18:01:14 +1000

    Added a c++ class to generate a solution

---
+++ VirtCamTest/Intermediate/Build/Win64/VirtCamTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/User/Documents/GitHub/VirtCamTest/VirtCamTest/Intermediate/Build/Win64/VirtCamTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
